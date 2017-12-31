92c7ae725bec5cdf38c4384f8dd1613f1a4a550a
Andy Lincoln <andy@andylincoln.io>
2017-12-31 00:20:59 -0500

    Sass and Javascript build with the beginnings of the Eventbrite API

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	res.setHeader("Access-Control-Allow-Origin", "www.eventbriteapi.com");
 	res.render('index', { title: 'Andy Lincoln & Sarah Croughwell' });
 });
 
