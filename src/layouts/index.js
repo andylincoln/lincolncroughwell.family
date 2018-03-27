9168760a09395dacb548a76e32eab8f1226deffb
andylincoln <andy@digitalartisans.com>
2018-03-26 22:18:16 -0400

    Reworking the build structure into Gatsby for images, sass, typekit, etc

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
-import './index.css'
+import "../scss/main.scss";
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
+    <Helmet>
+        <link rel="dns-prefetch" href="https://use.typekit.net"/>
+        <link rel="dns-prefetch" href="https://www.eventbriteapi.com/"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="robots" content="noindex"/>
+        <link rel="stylesheet" href="https://use.typekit.net/ivl3jxo.css"/>
+      </Helmet>
     <Header />
     <div
       style={{
