17df71bc98dc7d8875d5bf56b6c85744ca3f6c5c
Andy Lincoln <andrewlincoln11@gmail.com>
2018-04-01 22:06:58 -0400

    Menu button styles

--- src/components/FrontPageNav/front_page_nav.test.js
+++ src/components/FrontPageNav/front_page_nav.test.js
@@ -5,14 +5,16 @@ import FrontPageNav from './index';
 
 describe('FrontPageNav Component', function() {
   const nav = shallow(<FrontPageNav />);
-  
+
   //  <nav> element
   it('should render without issue"', function() {
     expect(nav.exists()).toBe(true);
   });
-  
+
   it('should be selectable by class ".main-navigation"', function() {
     expect(nav.is('.main-navigation')).toBe(true);
     expect(nav.is('.main-navigation--front-page')).toBe(true);
   });
+
+  // TODO Add test that all the links and buttons are present
 });
