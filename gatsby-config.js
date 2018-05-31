78c694b10ad9a08917df23a23fd36390f3df9694
Andy Lincoln <andrewlincoln11@gmail.com>
2018-05-31 16:35:46 -0400

    Front Page Icons, Backgrounds, layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,46 @@
-const autoprefixer = require("autoprefixer");
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   siteMetadata: {
-	title: 'Lincoln Croughwell Wedding',
+    title: 'Lincoln Croughwell Wedding',
   },
   plugins: [
-	'gatsby-plugin-react-helmet',
-	{
-	  resolve: "gatsby-source-filesystem",
-	  options: {
-		name: "img",
-		path: `${__dirname}/src/images`
-	  }
-	},
-	// {
-		// 	resolve: "gatsby-source-filesystem",
-		// 	options: {
-		// 		name: "img",
-		// 		path: `${__dirname}/static/assets`
-		// 	}
-		// },
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'img',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    // {
+    // 	resolve: "gatsby-source-filesystem",
+    // 	options: {
+    // 		name: "img",
+    // 		path: `${__dirname}/static/assets`
+    // 	}
+    // },
 
-	{
-			resolve: `gatsby-plugin-postcss-sass`,
-			options: {
-				postCssPlugins: [autoprefixer()]
-			}
-		},
-		"gatsby-transformer-sharp",
-		"gatsby-plugin-sharp",
-		// Netlify must be last
-		{
-			resolve: `gatsby-plugin-netlify`,
-			options: {
-				headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-				allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-				mergeSecurityHeaders: true, // boolean to turn off the default security headers
-				mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-				mergeCachingHeaders: true, // boolean to turn off the default caching headers
-				transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-				generateMatchPathRewrites: true // boolean to turn off automatic creation of redirect rules for client only paths
-			}
-		}
+    {
+      resolve: `gatsby-plugin-postcss-sass`,
+      options: {
+        postCssPlugins: [autoprefixer()],
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    // Netlify must be last
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
   ],
 };
--- src/components/FrontPageHero/index.js
+++ src/components/FrontPageHero/index.js
@@ -6,7 +6,10 @@ export default class FrontPageHero extends React.Component {
     return (
       <div className="front-page-hero landing-hero">
         <div className="front-page-hero front-page-hero--monogram">
-          <img src={monogram} alt="The monogram for the wedding, an S intertwined with an A"/>
+          <img
+            src={monogram}
+            alt="The monogram for the wedding, an S intertwined with an A"
+          />
         </div>
         <h1 className="front-page-hero--heading">{this.props.heading}</h1>
         <h2 className="front-page-hero--subheading">{this.props.subheading}</h2>
@@ -14,5 +17,3 @@ export default class FrontPageHero extends React.Component {
     );
   }
 }
-
-
