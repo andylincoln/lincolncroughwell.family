a194e1b90a71ca2052e046f6e50a237be7afe195
Andy Lincoln <andrew-lincoln@pluralsight.com>
2018-06-17 13:43:37 -0400

    Functioning front page nav

--- src/components/FrontPageNav/index.js
+++ src/components/FrontPageNav/index.js
@@ -4,33 +4,42 @@ import Link from 'gatsby-link';
 import Icon from '../Icon/';
 
 export default class FrontPageNav extends React.Component {
-  componentWillUpdate() {}
+	componentWillUpdate() {}
 
-  render() {
-    return (
-      <nav className="main-navigation main-navigation--front-page">
-        <button className="menu-button" aria-expanded="false">
-          <span className="a11y">Toggle to open or close the menu</span>
-          <Icon name="menu" />
-        </button>
-        <ul className="menu">
-          <li>
-            <Link to="/rsvp">RSVP</Link>
-          </li>
-          <li>
-            <Link to="/wedding-details">Wedding Details</Link>
-          </li>
-          <li>
-            <Link to="/registry">Registry</Link>
-          </li>
-          <li>
-            <Link to="/our-story">Our Story</Link>
-          </li>
-          <li>
-            <Link to="/things-to-do">Around Lowell</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+	menuButtonOnClick(e) {
+		const expanded = e.target.getAttribute('aria-expanded');
+		if (expanded === 'true') {
+			return e.target.setAttribute('aria-expanded', 'false');
+		}
+		return e.target.setAttribute('aria-expanded', 'true');
+	}
+
+	render() {
+		return (
+			<nav className="main-navigation main-navigation--front-page">
+				<button className="menu-button" aria-expanded="false" onClick={this.menuButtonOnClick.bind(this)}>
+					<span className="a11y">Toggle to open or close the menu</span>
+					<Icon name="menu" />
+					<Icon name="close" />
+				</button>
+				<ul className="menu">
+					<li>
+						<Link to="/rsvp">RSVP</Link>
+					</li>
+					<li>
+						<Link to="/wedding-details">Wedding Details</Link>
+					</li>
+					<li>
+						<Link to="/registry">Registry</Link>
+					</li>
+					<li>
+						<Link to="/our-story">Our Story</Link>
+					</li>
+					<li>
+						<Link to="/things-to-do">Around Lowell</Link>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,35 +1,35 @@
-import React from 'react'
+import React from 'react';
 import Link from 'gatsby-link';
 
 export default class Nav extends React.Component {
-  render() {
-    return (
-      <nav className="main-navigation">
-          <button className="menu-button">
-            <span className="a11y">Toggle to open or close the menu</span>
-            <div className="icon"></div>
-          </button>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/rsvp">RSVP</Link>
-            </li>
-            <li>
-              <Link to="/wedding-details">Wedding Details</Link>
-            </li>
-            <li>
-              <Link to="/registry">Registry</Link>
-            </li>
-            <li>
-              <Link to="/our-story">Our Story</Link>
-            </li>
-            <li>
-              <Link to="/things-to-do">Around Lowell</Link>
-            </li>
-          </ul>
-        </nav>
-    );
-  }
+	render() {
+		return (
+			<nav className="main-navigation">
+				<button className="menu-button">
+					<span className="a11y">Toggle to open or close the menu</span>
+					<div className="icon" />
+				</button>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/rsvp">RSVP</Link>
+					</li>
+					<li>
+						<Link to="/wedding-details">Wedding Details</Link>
+					</li>
+					<li>
+						<Link to="/registry">Registry</Link>
+					</li>
+					<li>
+						<Link to="/our-story">Our Story</Link>
+					</li>
+					<li>
+						<Link to="/things-to-do">Around Lowell</Link>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
 }
