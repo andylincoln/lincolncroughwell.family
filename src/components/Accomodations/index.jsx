13cd1e9311e8adf1ac3f9ab548d36b89f51773b9
Andy Lincoln <andrewlincoln11@gmail.com>
2018-07-23 01:15:42 -0400

    Stubbing out two column, accomodations styles

--- src/components/Accomodations/index.jsx
+++ src/components/Accomodations/index.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 export default class Accomodations extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return <section className="accomodations" />
   }
--- src/pages/wedding-details.js
+++ src/pages/wedding-details.js
@@ -7,6 +7,7 @@ import Accomodations from '../components/Accomodations'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import TwoCol from '../components/TwoCol'
 
 const WeddingDetailsPage = () => (
   <div>
@@ -21,7 +22,28 @@ const WeddingDetailsPage = () => (
     <hr />
     <Accomodations />
     <hr />
-    <section className="directions" id="directions" />
+    <TwoCol className="directions" id="directions">
+      <div>
+        <span className="h1">Hotel</span>
+        <p>
+          We have blocked some rooms for you at the UMass Lowell Inn and
+          Conference Center. Its in walking distance from the venue! When you
+          reserve your room mention that its our wedding.
+        </p>
+        <a href="tel:" className="phone-number">
+          Call 555-555-5555
+        </a>
+      </div>
+      <div>
+        <span className="h1">Transportation</span>
+        <p>
+          Although you can walk fromt he hotel to the venue, we understand that
+          if its your first time in the Mill City it can be a little
+          intimidating so we’ll have a shuttle that will make rounds from the
+          hotel to the venue throughout the course of the night
+        </p>
+      </div>
+    </TwoCol>
     <hr />
     <section className="wedding-party" id="wedding-party" />
     <Footer />
