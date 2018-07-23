610207e09ddaa1bd8b488266d5ee2faf405da334
Andy Lincoln <andrewlincoln11@gmail.com>
2018-07-23 00:51:59 -0400

    Accomodations and Two Col stubs

---
+++ src/components/Accomodations/index.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class Accomodations extends Component {
+  render() {
+    return <section className="accomodations" />
+  }
+}
---
+++ src/components/TwoCol/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link, { withPrefix } from 'gatsby-link'
+import FacebookGroupIcon from '../Icon/FacebookGroupIcon'
+import RSVPIcon from '../Icon/RSVPIcon'
+import RegistryIcon from '../Icon/RegistryIcon'
+
+const TwoCol = (props) => (
+  <div className="two-col">
+    <div className="col">{'Col1'}</div>
+    <div className="col">{'Col2'}</div>
+  </div>
+)
+
+export default TwoCol
